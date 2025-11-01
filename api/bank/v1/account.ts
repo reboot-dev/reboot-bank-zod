ba3a5f7c3ee704102742ee73543ed6ea0fbf6b34
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2025-11-01 01:34:23 +0100

    [Release] Synchronize for release (#20)

--- api/bank/v1/account.ts
+++ api/bank/v1/account.ts
@@ -1,52 +1,49 @@
+import { type API, reader, writer } from "@reboot-dev/reboot-api";
 import { z } from "zod/v4";
 
-export const api = {
-  Account: {
-    state: {
-      balance: z.number().default(0).meta({ tag: 2 }),
-    },
-
-    methods: {
-      balance: {
-        kind: "reader",
-        request: {},
-        response: {
-          amount: z.number().meta({ tag: 1 }),
-        },
-      },
-      deposit: {
-        kind: "writer",
-        request: {
-          amount: z.number().meta({ tag: 1 }),
-        },
-        response: z.void(),
-      },
-      withdraw: {
-        kind: "writer",
-        request: {
-          amount: z.number().meta({ tag: 1 }),
-        },
-        response: z.void(),
-        errors: [
-          z
-            .object({
-              type: z.literal("OverdraftError"),
-              amount: z.number().meta({ tag: 1 }),
-            })
-            .meta({ tag: 1 }),
-        ] as const,
-      },
-      open: {
-        kind: "writer",
-        factory: {},
-        request: {},
-        response: z.void(),
-      },
-      interest: {
-        kind: "writer",
-        request: {},
-        response: z.void(),
-      },
-    },
+export const Account = {
+  state: {
+    balance: z.number().default(0).meta({ tag: 1 }),
   },
+  methods: {
+    balance: reader({
+      request: {},
+      response: {
+        amount: z.number().meta({ tag: 1 }),
+      },
+    }),
+    deposit: writer({
+      request: {
+        amount: z.number().meta({ tag: 1 }),
+      },
+      response: z.void(),
+    }),
+    withdraw: writer({
+      request: {
+        amount: z.number().meta({ tag: 1 }),
+      },
+      response: z.void(),
+      errors: [
+        z
+          .object({
+            type: z.literal("OverdraftError"),
+            amount: z.number().meta({ tag: 1 }),
+          })
+          .meta({ tag: 1 }),
+      ] as const,
+    }),
+    open: writer({
+      factory: {},
+      request: {},
+      response: z.void(),
+    }),
+    interest: writer({
+      request: {},
+      response: z.void(),
+    }),
+  },
+};
+
+export const api: API = {
+  Account,
 };
